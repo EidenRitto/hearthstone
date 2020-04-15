b43fe33c5b4e274621059b83f303f132f9964a7a
EidenRitto <303713060@qq.com>
2020-04-15 18:03:37 +0800

    最后一点更新

--- src/main/java/cn/eiden/hsm/util/generator/SpellCardFileBuilder.java
+++ src/main/java/cn/eiden/hsm/util/generator/SpellCardFileBuilder.java
@@ -5,6 +5,7 @@ import cn.eiden.hsm.dbdata.Entity;
 import cn.eiden.hsm.game.Gamer;
 import cn.eiden.hsm.game.card.AbstractMagicCard;
 import cn.eiden.hsm.game.objct.Minion;
+import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
 import lombok.extern.slf4j.Slf4j;
@@ -25,16 +26,24 @@ public class SpellCardFileBuilder extends AbstractCardFileBuilder {
         CardInfo cardInfo = loadInCache(entity);
 
         final String fileName = formatFileName(cardInfo.getCardName());
-
+//        https://github.com/square/javapoet/
         TypeSpec myClass = TypeSpec.classBuilder(fileName)
                 .superclass(AbstractMagicCard.class)
                 .addJavadoc("本java源文件由java poet自动生成\n")
-                .addJavadoc("@author $L\n",AUTHOR)
+                .addJavadoc("@author $L\n", AUTHOR)
                 .addJavadoc("@date $L", DTF.format(LocalDateTime.now()))
                 .addModifiers(Modifier.PUBLIC)
+                .addField(FieldSpec.builder(int.class, "COST")
+                        .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+                        .initializer("$L",cardInfo.getCost())
+                        .build())
+                .addField(FieldSpec.builder(String.class, "DESCRIPTION")
+                        .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+                        .initializer("$S",cardInfo.getCardText())
+                        .build())
                 .addMethod(MethodSpec.constructorBuilder()
                         .addModifiers(Modifier.PUBLIC)
-                        .addParameter(String.class, "greeting")
+                        //super(COST, DESCRIPTION, CARD_NAME, Profession.Neutral);
                         .addStatement("this.$N = $N", "greeting", "greeting")
                         .build())
                 .addMethod(MethodSpec.methodBuilder("magicEffect")
